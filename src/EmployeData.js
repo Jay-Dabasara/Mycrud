46f1f321866fc5ef530fa00a6c4b60fea3cd1b44
Jay Dabasara <103643217+Jay-Dabasara@users.noreply.github.com>
2025-05-19 11:50:51 +0530

    Add files via upload

---
+++ src/EmployeData.js
@@ -0,0 +1,29 @@
+export const EmployeData = [
+    {
+        id: 1,
+        firstName: 'virat',
+        lastName: 'kohli',
+        age: 40
+    },
+
+    {
+        id: 2,
+        firstName: 'Gayle',
+        lastName: 'crish',
+        age: 45
+    },
+
+    {
+        id: 3,
+        firstName: 'rohit',
+        lastName: 'sharma',
+        age: 44
+    },
+
+    {
+        id: 4,
+        firstName: 'Hardik',
+        lastName: 'Pandya',
+        age: 34
+    }
+]
